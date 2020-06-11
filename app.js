b304746d05ed47519ab382843c810fa2439fedf6
Donghyeon Kim <0916dhkim@gmail.com>
2020-06-11 10:20:32 +0900

    Implement Get Operation

--- app.js
+++ app.js
@@ -14,6 +14,17 @@ app.get("/", (req, res) => {
     res.send("Hello, World!");
 });
 
+app.get("/todo", async (req, res, next) => {
+    db.any("SELECT * FROM todo")
+        .then(data => res.send(data))
+        .catch(() => {
+            res.status(500);
+            res.send({
+                error: "Database error."
+            });
+        });
+});
+
 app.listen(PORT, () => {
     console.log(`App is listening on port ${PORT}.`);
 });
