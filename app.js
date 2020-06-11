5e51ec190b952cdbfe6503ee0358e01eda206543
Donghyeon Kim <0916dhkim@gmail.com>
2020-06-11 09:40:42 +0900

    Open Postgres Connection

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const pgp = require("pg-promise")();
 const app = express();
 const PORT = process.env.PORT || 3000;
+const connection = {
+    host: "db",
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD
+};
+const db = pgp(connection);
 
 app.get("/", (req, res) => {
     res.send("Hello, World!");
